698a4b14303c1a2152c6af306e7133b643b19174
jamesu <jamesu@gmail.com>
2010-03-04 16:48:07 +0000

    Expose more to the groups interface

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,16 @@
 module GroupsHelper
+  def groups_column
+    render :partial => 'groups/column', :locals => {
+      :group => @group,
+      :groups => current_user.groups }
+  end
+  
+  def list_groups(groups)
+    render :partial => 'groups/group', :collection => groups
+  end
+  
+  def group_icon(group)
+    src = group.has_logo? ? group.logo.url(:icon) : "/images/icon_default.png"
+    link_to "<img class='icon' src='#{src}' alt='#{group.name}'/>", group_path(group)
+  end
 end
