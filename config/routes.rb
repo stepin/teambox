1643de4959acf593b9bed0319657a334df724fca
jamesu <jamesu@gmail.com>
2010-03-05 16:51:39 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -80,7 +80,9 @@ ActionController::Routing::Routes.draw do |map|
     project.resources :pages, :has_many => [:notes,:dividers,:task_list,:uploads], :member => { :reorder => :post }
   end
   
-  map.resources :groups, :member => { :logo => :any }
+  map.resources :groups, :member => { :logo => :any, :projects => :any, :members => :any} do |group|
+    group.resources :invitations, :member => [:accept,:decline,:resend]
+  end
   
   # map.resources :comments
   map.resources :task_lists, :only => [ :index ]
